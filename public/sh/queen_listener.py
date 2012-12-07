99c75815754efda18fe33876c687ff1e2720bdc0
Tim Cameron Ryan <id@timryan.org>
2012-12-07 15:42:41 -0500

    Burst

--- public/sh/queen_listener.py
+++ public/sh/queen_listener.py
@@ -7,7 +7,7 @@ import subprocess
 arduino = serial.Serial('/dev/tty.usbmodem1411', 57600) # serial port# and baud rate
 
 def post_to_server(queen_id, ant_id):
-  subprocess.call(["curl", "-X", "POST", "-d", "hill=" + hill_id, "-d", "ant=" + ant_id, "api.olinexpo.com/hardware"])
+  subprocess.call(["curl", "-X", "POST", "-d", "colony=" + colony_id, "-d", "ant=" + ant_id, "api.olinexpo.com/hardware"])
 
 prefix = 'Got payload: '
 while 1:
@@ -18,9 +18,9 @@ while 1:
     print "message:", message
     # 8 bytes, each 2 characters big, so make minimum 16 to left pad with appropriate leading 0's
     # these might be reversed
-    hill_id = message[0:8]
+    colony_id = message[0:8]
     ant_id = message[8:]
-    print "hill:", hill_id, "ant:", ant_id
-    post_to_server(hill_id, ant_id);
+    print "colony:", colony_id, "ant:", ant_id
+    post_to_server(colony_id, ant_id);
     # subprocess.check_output(["echo", "Hello World!"])
 
