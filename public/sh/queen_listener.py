9de7453f796c7ba0f663ae77025e3f8861a21edc
Tim Cameron Ryan <id@timryan.org>
2012-12-09 13:26:42 -0500

    Update SH script to listen for correct ID endianness

--- public/sh/queen_listener.py
+++ public/sh/queen_listener.py
@@ -4,7 +4,7 @@ import serial, json
 import os
 import subprocess
 
-arduino = serial.Serial('/dev/tty.usbmodem1411', 57600) # serial port# and baud rate
+arduino = serial.Serial('/dev/ttyACM0', 57600) # serial port# and baud rate
 
 def chunks(l, n):
   return [l[i:i+n] for i in range(0, len(l), n)]
@@ -25,8 +25,12 @@ while 1:
     ant_id = message[8:]
 
     # Revert to small-endian.
-    colony_id = ''.join(chunks(colony_id, 2).reverse())
-    ant_id = ''.join(chunks(ant_id, 2).reverse())
+    colony_id = chunks(colony_id, 2)
+    colony_id.reverse()
+    colony_id = ''.join(colony_id)
+    ant_id = chunks(colony_id, 2)
+    ant_id.reverse()
+    ant_id = ''.join(colony_id)
 
     # POST that biznatch
     print "colony:", json.dumps(colony_id), "ant:", json.dumps(ant_id)
