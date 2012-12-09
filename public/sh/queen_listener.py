507123e93bb0de3fa9ec50fd49ef31980efccd13
Tim Cameron Ryan <id@timryan.org>
2012-12-08 23:26:57 -0500

    Big endian

--- public/sh/queen_listener.py
+++ public/sh/queen_listener.py
@@ -6,6 +6,9 @@ import subprocess
 
 arduino = serial.Serial('/dev/tty.usbmodem1411', 57600) # serial port# and baud rate
 
+def chunks(l, n):
+  return [l[i:i+n] for i in range(0, len(l), n)]
+
 def post_to_server(queen_id, ant_id):
   subprocess.call(["curl", "-X", "POST", "-d", "colony=" + colony_id, "-d", "ant=" + ant_id, "api.olinexpo.com/hardware"])
 
@@ -20,7 +23,12 @@ while 1:
     # these might be reversed
     colony_id = message[0:8]
     ant_id = message[8:]
+
+    # Revert to small-endian.
+    colony_id = ''.join(chunks(colony_id, 2).reverse())
+    ant_id = ''.join(chunks(ant_id, 2).reverse())
+
+    # POST that biznatch
     print "colony:", json.dumps(colony_id), "ant:", json.dumps(ant_id)
     post_to_server(colony_id, ant_id);
     # subprocess.check_output(["echo", "Hello World!"])
-
