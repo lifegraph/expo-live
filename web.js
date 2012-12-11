f21e8c90a1a6b01093d738b2a0cef13f800edc99
Tim Cameron Ryan <id@timryan.org>
2012-12-10 22:51:52 -0500

    CORS

--- web.js
+++ web.js
@@ -26,6 +26,12 @@ var SEGMENT_THRESHOLD = 1*60*1000;
  */
 
 var app = express();
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
