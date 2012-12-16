71761961ab07659ba4b0685c6b591445d64999bf
Tim Cameron Ryan <id@timryan.org>
2012-12-16 16:12:39 -0500

    Fucked up

--- public/sh/queen_listener.py
+++ public/sh/queen_listener.py
@@ -10,7 +10,7 @@ def chunks(l, n):
   return [l[i:i+n] for i in range(0, len(l), n)]
 
 def post_to_server(queen_id, ant_id, ping_id):
-  subprocess.call(["curl", "-X", "POST", "-d", "colony=" + colony_id, "-d", "ant=" + ant_id, "-d", "ping=" + ping_id, "localhost:5000/binds"])
+  subprocess.call(["curl", "-X", "POST", "-d", "colony=" + colony_id, "-d", "ant=" + ant_id, "-d", "ping=" + ping_id, "api.olinexpo.com/binds"])
 
 def append_to_file(filename, data):
   with open(filename, "a") as f: # open file in append mode
