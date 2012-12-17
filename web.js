b8c5735fac0daf0107c9338474555cacd9303cfe
Tim Cameron Ryan <id@timryan.org>
2012-12-17 07:22:30 -0500

    Debug live

--- web.js
+++ web.js
@@ -711,7 +711,7 @@ function setupMongo (next) {
 var PRESCACHE = [];
 
 function getPresentationByLocation (date, locid) {
-  var hours = time.getHours() - 5;
+  var hours = date.getHours() - 5;
   return PRESCACHE.filter(function (row) {
     return row.start_hour == hours && row.room == locid;
   })[0];
